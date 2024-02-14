8fcf5b75619812eb1824cd17d31bcb2de92bf63c
rafengelen <rafengelen01@gmail.com>
2024-02-14 10:00:47 +0100

    created ng project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: '**', component: PageNotFoundComponent },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'o-accent-blue': "#04C4D9",
+        'o-accent-orange': '#FD6B5E',
+        'o-light-orange': '#FF9181',
+        'o-cyan':'#005F7A',
+        'o-dark-blue': "#041624" 
+      },
+    },
+  },
+  plugins: [],
+}
+/*
+#04C4D9
+#041624
+#005F7A
+#FF9181
+#FD6B5E
+*/
